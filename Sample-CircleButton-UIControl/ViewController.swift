548723a52480e0ef20978be65d34dc07ca28ebda
takumi314 <isospin_symmetry@me.com>
2017-05-28 03:56:06 +0900

    implement the button developed with CricleView

--- Sample-CircleButton-UIControl/ViewController.swift
+++ Sample-CircleButton-UIControl/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillLayoutSubviews() {
+        let baseView = createCircleView()
+        self.view.addSubview(baseView)
+        self.view.backgroundColor = .black
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Provate methods
+
+    private func createCircleView() -> UIView {
+        let circleView = CircleView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let baseView = UIView(frame: circleView.bounds)
+        baseView.backgroundColor = .clear
+        baseView.center = self.view.center
+        baseView.addSubview(circleView)
+
+        return baseView
     }
 
 
