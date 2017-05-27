869042b5f0e1de16854c5fbb6e7d44d412839e68
takumi314 <isospin_symmetry@me.com>
2017-05-27 18:09:07 +0900

    make new class inherited from UIControl

---
+++ Sample-CircleButton-UIControl/CircleView.swift
@@ -0,0 +1,15 @@
+//
+//  CircleView.swift
+//  Sample-CircleButton-UIControl
+//
+//  Created by NishiokaKohei on 2017/05/27.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - UIControl
+
+class CircleView: UIControl {
+    
+}
