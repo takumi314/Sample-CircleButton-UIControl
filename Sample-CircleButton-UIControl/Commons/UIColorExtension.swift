a6171d7e6578cd67af8edb6785de43df9b8f1330
takumi314 <isospin_symmetry@me.com>
2017-05-27 17:05:54 +0900

    make initializer with  and Alpha value[D

--- Sample-CircleButton-UIControl/Commons/UIColorExtension.swift
+++ Sample-CircleButton-UIControl/Commons/UIColorExtension.swift
@@ -10,4 +10,20 @@ import UIKit
 
 extension UIColor {
 
+    // MARK: - Initializer
+
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let mask = 0x000000FF
+
+        let r = Int(hex >> 16) & mask
+        let g = Int(hex >> 8) & mask
+        let b = Int(hex) & mask
+
+        let red = CGFloat(r) / 255
+        let green = CGFloat(g) / 255
+        let blue = CGFloat(b) / 255
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
 }
