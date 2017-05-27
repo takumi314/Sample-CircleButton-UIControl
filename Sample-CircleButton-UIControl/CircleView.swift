3729a69c65c6f5fba1426349a843c4c2a7f58d2c
takumi314 <isospin_symmetry@me.com>
2017-05-27 18:29:13 +0900

    add new definition for shaping the ciricle

--- Sample-CircleButton-UIControl/CircleView.swift
+++ Sample-CircleButton-UIControl/CircleView.swift
@@ -11,5 +11,18 @@ import UIKit
 // MARK: - UIControl
 
 class CircleView: UIControl {
-    
+
+
+    // MARK: - Shape
+
+    lazy var circleShapedLayer: CAShapeLayer = {
+        let path = UIBezierPath(ovalIn: self.bounds)
+
+        let circleLayer = CAShapeLayer()
+        circleLayer.path = path.cgPath
+        // 色の指定
+
+        return circleLayer
+    }()
+
 }
