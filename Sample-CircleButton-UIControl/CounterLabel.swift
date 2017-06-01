590b25f90b6c90c440bbe18f27b264b32c28f3f0
takumi314 <isospin_symmetry@me.com>
2017-06-02 00:32:46 +0900

    make new class fot UILabel

---
+++ Sample-CircleButton-UIControl/CounterLabel.swift
@@ -0,0 +1,13 @@
+//
+//  CounterLabel.swift
+//  Sample-CircleButton-UIControl
+//
+//  Created by NishiokaKohei on 2017/06/02.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class CounterLabel: UILabel {
+
+}
