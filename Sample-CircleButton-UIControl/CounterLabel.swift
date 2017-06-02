7fead6019d2b49432d0a6fca3c94f95035bde279
takumi314 <isospin_symmetry@me.com>
2017-06-02 09:07:42 +0900

    add the methods to initialise and to make a configuration,

--- Sample-CircleButton-UIControl/CounterLabel.swift
+++ Sample-CircleButton-UIControl/CounterLabel.swift
@@ -10,4 +10,32 @@ import UIKit
 
 class CounterLabel: UILabel {
 
+    // MARK: - Initialzer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = frame
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+
+    func writeText(_ text: String) {
+        self.text = text
+    }
+
+    func config()  {
+        self.backgroundColor = .clear
+        self.textColor = .black
+        self.textAlignment = .center
+        self.font = UIFont.systemFont(ofSize: 40)
+    }
+
+    func eraseText() {
+        self.text = ""
+    }
+
 }
